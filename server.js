6680cc00e3482c28b62c543d85bd6f5e68af176a
Atila Romero <atilaromero@gmail.com>
2019-05-28 14:19:51 -0300

    Dockerfile; REST_URL

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const graphqlHTTP = require('express-graphql')
 const {typeDefs, get_resolvers} = require('./schema')
 const {makeExecutableSchema} = require('graphql-tools')
 
+const REST_URL = process.env.REST_URL || "http://localhost"
+
 const schema = makeExecutableSchema({
     typeDefs,
-    resolvers: get_resolvers('http://wekan.triagem1'),
+    resolvers: get_resolvers(REST_URL),
 })
 
 const app = express()
-app.use('/graphql', graphqlHTTP({
+app.use('/', graphqlHTTP({
   schema: schema,
   rootValue: null,
   graphiql: true,
